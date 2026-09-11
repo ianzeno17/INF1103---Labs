6f7246754b10e3f10a424abdca29b8e02cda2c16
Shiqi <leeshiqi4@gmail.com>
2026-09-11 15:48:16 +0800

    Reporting

--- auditor.py
+++ auditor.py
@@ -1,18 +1,23 @@
 inventory = 0
+failed_entries = 0
 
 while True:
     stock = input("Enter stock quantity: ")
 
     if stock == "quit":
+        print("Total Units Processed", inventory)
+        print("Number of Failed/Rejected Entries:", failed_entries)
         break
     elif not stock.isdigit():
         print("Please enter an integer.")
+        failed_entries += 1
         continue
 
     stock = int(stock)
 
     if stock < 0:
         print("Please enter a positive integer")
+        failed_entries += 1
         continue
 
     inventory += stock
