972aef1fd7e796472dcb8ebca05b70f687cc8b3a
Shiqi <leeshiqi4@gmail.com>
2026-09-19 15:20:45 +0800

    Altering main loop

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,32 +1,3 @@
-inventory = 0
-failed_entries = 0
-
-while True:
-    stock = input("Enter stock quantity: ")
-
-    if stock == "quit":
-        print("Total Units Processed", inventory)
-        print("Number of Failed/Rejected Entries:", failed_entries)
-        break
-    elif not stock.isdigit():
-        print("Please enter an integer.")
-        failed_entries += 1
-        continue
-
-    stock = int(stock)
-
-    if stock < 0:
-        print("Please enter a positive integer")
-        failed_entries += 1
-        continue
-
-    inventory += stock
-
-    if inventory > 500:
-        print("ALERT: Inventory has exceeded 500 units")
-        break
-
-
 def get_valid_input():
     stock = input("Enter stock quantity: ")
 
@@ -38,3 +9,24 @@ def get_valid_input():
         return None
 
     return int(stock)
+
+
+inventory = 0
+failed_entries = 0
+
+while True:
+    stock = get_valid_input()
+
+    if stock == "quit":
+        print("Total Units Processed", inventory)
+        print("Number of Failed/Rejected Entries:", failed_entries)
+        break
+    elif stock is None:
+        failed_entries += 1
+        continue
+
+    inventory += stock
+
+    if inventory > 500:
+        print("ALERT: Inventory has exceeded 500 units")
+        break
