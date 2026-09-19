dadef4d808a3954d9f9b64a6100117e2d1354706
Shiqi <leeshiqi4@gmail.com>
2026-09-19 15:28:31 +0800

    Add calculate_tax() function

--- modular_auditor.py
+++ modular_auditor.py
@@ -14,6 +14,10 @@ def process_delivery(current_total, new_value):
     new_total = current_total + new_value
     return new_total
 
+def calculate_tax(amount):
+    tax = amount * 0.10
+    return tax
+
 
 inventory = 0
 failed_entries = 0
